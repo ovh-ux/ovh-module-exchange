32311cef070cc64c5c158d4892e63daa9d2f0481
euhmeuh <jerome.martin@corp.ovh.com>
2017-10-11 18:25:28 +0200

    feat(accounts): integrate legacy hold on popup UI

--- src/exchange/account/archive/account-legacy-hold-on.controller.js
+++ src/exchange/account/archive/account-legacy-hold-on.controller.js
@@ -10,6 +10,18 @@ angular
                 $rootScope,
                 exchangeStates
             };
+
+            this.account = navigation.currentActionData;
+            this.value = angular.copy(this.account.litigation);
+            this.days = angular.copy(this.account.litigationPeriod);
+            this.unlimited = this.days === 0;
+
+            this.firstTime = !this.value;
+        }
+
+        needsUpdate () {
+            return this.value !== this.account.litigation ||
+                this.days !== this.account.litigationPeriod;
         }
 
         update () {
