9d779aa7bda896ba72e8f95c81038174915d9034
euhmeuh <jerome.martin@corp.ovh.com>
2017-10-12 10:55:18 +0200

    connect legacy hold on popup to back

--- src/exchange/account/archive/account-legacy-hold-on.controller.js
+++ src/exchange/account/archive/account-legacy-hold-on.controller.js
@@ -1,30 +1,57 @@
 angular
     .module("Module.exchange.controllers")
     .controller("ExchangeAccountLegacyHoldOnCtrl", class ExchangeAccountLegacyHoldOnCtrl {
-        constructor ($scope, Exchange, ExchangeAccountService, navigation, $rootScope, exchangeStates) {
+        constructor ($scope, Exchange, ExchangeAccountService, navigation, $rootScope, messaging, translator, exchangeStates) {
             this.services = {
                 $scope,
                 Exchange,
                 ExchangeAccountService,
                 navigation,
                 $rootScope,
+                messaging,
+                translator,
                 exchangeStates
             };
 
+            $scope.update = () => this.update();
+
+            this.$routerParams = Exchange.getParams();
             this.account = navigation.currentActionData;
-            this.value = angular.copy(this.account.litigation);
-            this.days = angular.copy(this.account.litigationPeriod);
+            this.value = this.originalValue = angular.copy(this.account.litigation);
+            this.days = this.originalDays = angular.copy(this.account.litigationPeriod) || 0;
             this.unlimited = this.days === 0;
 
             this.firstTime = !this.value;
         }
 
+        updateUnlimited () {
+            if (this.unlimited) {
+                this.days = 0;
+            } else {
+                this.days = this.originalDays;
+            }
+        }
+
         needsUpdate () {
-            return this.value !== this.account.litigation ||
-                this.days !== this.account.litigationPeriod;
+            return this.value !== this.originalValue ||
+                this.days !== this.originalDays;
+        }
+
+        applyValues (account) {
+            account.litigation = this.value;
+            account.litigationPeriod = !this.value || this.unlimited ? 0 : this.days;
+            return account;
         }
 
         update () {
-
+            if (this.needsUpdate()) {
+                this.services.Exchange.updateAccount(this.$routerParams.organization, this.$routerParams.productId, this.applyValues(this.account)).then((data) => {
+                    this.services.messaging.writeSuccess(this.services.translator.tr("exchange_ACTION_legacy_hold_on_success_message"));
+                }).catch((failure) => {
+                    this.services.messaging.writeError(this.services.translator.tr("exchange_common_error"), failure);
+                }).finally(() => {
+                    this.services.navigation.resetAction();
+                });
+            }
         }
     });
