e4258a9e582d481af89df3f8daf84ca60b29c419
DakuTenshi <dakutenshi@gmail.com>
2017-10-30 12:12:39 +0100

    :lipstick: style(accountDelegation): this one was okay

--- src/exchange/account/delegation/account-delegation.controller.js
+++ src/exchange/account/delegation/account-delegation.controller.js
@@ -88,16 +88,12 @@ angular
                         message: datum,
                         type: "PARTIAL"
                     });
-
-                    return false;
                 } else if (datum.status === "ERROR") {
                     datum.message = this.services.translator.tr(`exchange_tab_TASKS_${datum.function}`);
                     datum.type = "ERROR";
                     state = "PARTIAL";
                     numberOfErrors++;
                 }
-
-                return true;
             });
 
             if (numberOfErrors === data.length) {
