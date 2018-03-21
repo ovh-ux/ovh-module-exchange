fcf5ecf726fc2b4846f197c42d47607e4cd14232
Marie Jones <marie.jones@corp.ovh.com>
2018-03-21 15:36:50 +0100

    :ambulance: fix(email-creation.add): Fix error message template
    Insert template directly instead of using a component to prevent loading lasting more than the window timeout
    If not, scrolling function is called before the component is displayed

--- src/exchange/wizard-hosted-creation/wizard-message/wizard-message.component.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module("Module.exchange.components")
-    .component("exchangeWizardHostedCreationWizardMessage", {
-        templateUrl: "exchange/wizard-hosted-creation/wizard-message/wizard-message.html",
-        require: {
-            homepage: "^^exchangeWizardHostedCreation"
-        }
-    });
-
