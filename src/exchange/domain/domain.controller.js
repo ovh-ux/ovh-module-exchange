a86f849be74668165bf522bc1ad7a74b057afd5a
TedKac <42211327+TedKac@users.noreply.github.com>
2018-10-24 14:10:00 +0200

    refactor(exchange.domain): use oui-status

--- src/exchange/domain/domain.controller.js
+++ src/exchange/domain/domain.controller.js
@@ -2,22 +2,28 @@ angular.module('Module.exchange.controllers').controller(
   'ExchangeTabDomainsCtrl',
   class ExchangeTabDomainsCtrl {
     constructor(
-      $scope,
       $http,
+      $scope,
+      $translate,
       Exchange,
       ExchangeDomains,
-      $translate,
-      exchangeStates,
       exchangeServiceInfrastructure,
+      exchangeStates,
+      exchangeVersion,
+      messaging,
+      navigation,
     ) {
       this.services = {
-        $scope,
         $http,
+        $scope,
+        $translate,
         Exchange,
         ExchangeDomains,
-        $translate,
-        exchangeStates,
         exchangeServiceInfrastructure,
+        exchangeStates,
+        exchangeVersion,
+        messaging,
+        navigation,
       };
 
       this.$routerParams = Exchange.getParams();
@@ -122,6 +128,31 @@ angular.module('Module.exchange.controllers').controller(
       }
 
       return false;
+    }
+
+    isReseller2010AuthInvalidMx() {
+      return (
+        this.services.exchangeServiceInfrastructure.isProvider()
+        && this.services.exchangeVersion.isVersion(2010)
+      );
+    }
+
+    isUpdateDisabled(domain) {
+      return (
+        !this.services.exchangeStates.constructor.isOk(domain)
+        || domain.taskInProgress
+        || this.isReseller2010AuthInvalidMx()
+      );
+    }
+
+    isDeleteDisabled(domain) {
+      return !this.services.exchangeStates.constructor.isOk(domain) || domain.accountsCount > 0;
+    }
+
+    getDeleteTooltip(domain) {
+      return this.isDeleteDisabled(domain)
+        ? this.services.$translate.instant('exchange_tab_domain_delete_domain_accounts_warning')
+        : '';
     }
   },
 );
