e55803863270fdfec3c4c32aeb90a70c7d4207b0
cyril.biencourt <cyril.biencourt@corp.ovh.com>
2019-07-18 08:20:10 +0200

    feat(security): add service mfa support

--- src/exchange/security/security.controller.js
+++ src/exchange/security/security.controller.js
@@ -1,7 +1,8 @@
 angular.module('Module.exchange.services').controller(
   'ExchangeServicesConfigureCtrl',
   class ExchangeServicesConfigureCtrl {
-    constructor($scope, APIExchange, Exchange, $translate, navigation, messaging) {
+    constructor($q, $scope, APIExchange, Exchange, $translate, navigation, messaging) {
+      this.$q = $q;
       this.services = {
         $scope,
         APIExchange,
@@ -23,44 +24,54 @@ angular.module('Module.exchange.services').controller(
       this.formIsValid = {
         value: true,
       };
+    }
 
+    $onInit() {
       this.retrievingDetails();
-
-      // $scope.submitting = () => this.submitting();
-      // $scope.isFormValid = () => this.formIsValid.value;
     }
 
     retrievingDetails() {
-      return this.services.APIExchange.get('/{organizationName}/service/{exchangeService}', {
-        urlParams: {
-          organizationName: this.exchange.organization,
-          exchangeService: this.exchange.domain,
-        },
-      })
-        .then((serviceDescription) => {
-          this.service = serviceDescription;
-          this.service.lockoutThreshold = _.isNumber(this.service.lockoutThreshold)
-            ? this.service.lockoutThreshold
-            : 0;
-          this.service.minPasswordLength = _.isNumber(this.service.minPasswordLength)
-            ? this.service.minPasswordLength
-            : 0;
-          this.service.minPasswordAge = _.isNumber(this.service.minPasswordAge)
-            ? this.service.minPasswordAge
-            : 0;
-          this.service.maxPasswordAge = _.isNumber(this.service.maxPasswordAge)
-            ? this.service.maxPasswordAge
-            : 0;
-          this.service.passwordHistoryCount = _.isNumber(this.service.passwordHistoryCount)
-            ? this.service.passwordHistoryCount
-            : 0;
+      return this.$q.all({
+        serviceDescription: this.services.APIExchange.get('/{organizationName}/service/{exchangeService}', {
+          urlParams: {
+            organizationName: this.exchange.organization,
+            exchangeService: this.exchange.domain,
+          },
+        }),
+        server: this.services.Exchange.getExchangeServer(
+          this.exchange.organization,
+          this.exchange.domain,
+        ),
+      }).then(({ serviceDescription, server }) => {
+        this.service = serviceDescription;
 
-          this.originalService = _.clone(this.service);
-          return this.service;
-        })
-        .finally(() => {
-          this.loaders.details = false;
-        });
+        this.service.lockoutThreshold = _.isNumber(this.service.lockoutThreshold)
+          ? this.service.lockoutThreshold
+          : 0;
+        this.service.minPasswordLength = _.isNumber(this.service.minPasswordLength)
+          ? this.service.minPasswordLength
+          : 0;
+        this.service.minPasswordAge = _.isNumber(this.service.minPasswordAge)
+          ? this.service.minPasswordAge
+          : 0;
+        this.service.maxPasswordAge = _.isNumber(this.service.maxPasswordAge)
+          ? this.service.maxPasswordAge
+          : 0;
+        this.service.passwordHistoryCount = _.isNumber(this.service.passwordHistoryCount)
+          ? this.service.passwordHistoryCount
+          : 0;
+
+        this.originalService = _.clone(this.service);
+        this.owaMfa = server.owaMfa;
+        return this.service;
+      }).catch((err) => {
+        this.services.messaging.writeError(
+          this.services.$translate.instant('exchange_common_error'),
+          err,
+        );
+      }).finally(() => {
+        this.loaders.details = false;
+      });
     }
 
     resetValues() {
@@ -86,11 +97,20 @@ angular.module('Module.exchange.services').controller(
         dataToSend.lockoutObservationWindow = this.service.lockoutObservationWindow;
       }
 
-      return this.services.Exchange.setConfiguration(
-        this.exchange.organization,
-        this.exchange.domain,
-        dataToSend,
-      )
+      return this.$q.all({
+        configuration: this.services.Exchange.setConfiguration(
+          this.exchange.organization,
+          this.exchange.domain,
+          dataToSend,
+        ),
+        owaMfa: this.owaMfa === _.get(this.exchange, 'serverDiagnostic.owaMfa')
+          ? this.$q.when(true)
+          : this.services.Exchange.updateExchangeServer(
+            this.exchange.organization,
+            this.exchange.domain,
+            { owaMfa: this.owaMfa },
+          ),
+      })
         .then(() => {
           this.services.Exchange.resetAccounts();
           this.services.messaging.writeSuccess(
@@ -107,182 +127,6 @@ angular.module('Module.exchange.services').controller(
           this.services.navigation.resetAction();
           this.loaders.put = false;
         });
-    }
-
-    setValidity() {
-      this.formIsValid.value = this.serviceForm.$valid;
-    }
-
-    static minPasswordLengthCheck(input) {
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-
-      const intValue = this.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        if (intValue !== 0) {
-          input.$setValidity('min', intValue >= 3);
-        }
-
-        input.$setValidity('max', intValue <= 14);
-      }
-    }
-
-    passwordHistoryCountCheck(input) {
-      if (this.service.maxPasswordAge <= 0) {
-        input.$setValidity('min', true);
-        input.$setValidity('max', true);
-        input.$setValidity('number', true);
-        input.$setValidity('pattern', true);
-        input.$setValidity('required', true);
-      }
-    }
-
-    static minPasswordAgeCheck(input) {
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-      input.$setValidity('minToBigForMax', true);
-
-      const intValue = this.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        if (this.service.maxPasswordAge === 0) {
-          input.$setValidity('min', intValue >= 0);
-          input.$setValidity('max', intValue <= 90);
-        } else if (intValue !== 0) {
-          input.$setValidity('minToBigForMax', intValue < +this.service.maxPasswordAge);
-        }
-      }
-    }
-
-    maxPasswordAgeCheck(input) {
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-      input.$setValidity('maxToSmallForMin', true);
-
-      const intValue = ExchangeServicesConfigureCtrl.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        input.$setValidity('min', intValue >= 0);
-        input.$setValidity('max', intValue <= 90);
-
-        if (intValue !== 0) {
-          input.$setValidity('maxToSmallForMin', intValue > +this.service.minPasswordAge);
-        }
-      }
-
-      if (this.serviceForm.passwordHistoryCount != null) {
-        this.serviceForm.passwordHistoryCount.$validate();
-      }
-    }
-
-    lockoutThresholdCheck(input) {
-      input.$setValidity('required', true);
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-
-      const intValue = ExchangeServicesConfigureCtrl.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        input.$setValidity('min', intValue >= 0);
-        input.$setValidity('max', intValue <= 14);
-      } else {
-        input.$setValidity('required', false);
-      }
-
-      if (
-        this.serviceForm.lockoutObservationWindow != null
-        && this.serviceForm.lockoutDuration != null
-      ) {
-        this.lockoutObservationWindowCheck(this.serviceForm.lockoutObservationWindow);
-        this.lockoutDurationCheck(this.serviceForm.lockoutDuration);
-      }
-    }
-
-    lockoutObservationWindowCheck(input, comesFromLockoutDurationCheck = false) {
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-      input.$setValidity('toBigForDuration', true);
-
-      if (this.serviceForm.lockoutThreshold.$valid && this.service.lockoutThreshold === 0) {
-        input.$setValidity('number', true);
-        input.$setValidity('mustBeInteger', true);
-        return;
-      }
-
-      const intValue = ExchangeServicesConfigureCtrl.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        input.$setValidity('min', intValue >= 1);
-        input.$setValidity('max', intValue <= 90);
-
-        if (_.isNumber(this.service.lockoutDuration) && !_.isNaN(this.service.lockoutDuration)) {
-          input.$setValidity('toBigForDuration', intValue <= this.service.lockoutDuration);
-        }
-      }
-
-      if (!comesFromLockoutDurationCheck) {
-        this.lockoutDurationCheck(this.serviceForm.lockoutDuration, true);
-      }
-    }
-
-    lockoutDurationCheck(input, comesFromLockoutObservationWindowCheck = false) {
-      input.$setValidity('required', true);
-      input.$setValidity('min', true);
-      input.$setValidity('max', true);
-
-      if (this.serviceForm.lockoutThreshold.$valid && this.service.lockoutThreshold === 0) {
-        input.$setValidity('number', true);
-        input.$setValidity('mustBeInteger', true);
-        return;
-      }
-
-      const intValue = ExchangeServicesConfigureCtrl.convertToNumber(input);
-      if (_.isNumber(intValue)) {
-        input.$setValidity(
-          'min',
-          intValue >= this.service.lockoutObservationWindow && intValue > 0,
-        );
-        input.$setValidity('max', intValue <= 90);
-      } else {
-        input.$setValidity('required', false);
-      }
-
-      if (!comesFromLockoutObservationWindowCheck) {
-        this.lockoutObservationWindowCheck(this.serviceForm.lockoutObservationWindow, true);
-      }
-    }
-
-    static convertToNumber(input) {
-      input.$setValidity('number', true);
-      input.$setValidity('mustBeInteger', true);
-      const value = input.$viewValue;
-
-      try {
-        const intValue = parseInt(value, 10);
-        if (_.isFinite(intValue)) {
-          if (Number.isInteger(intValue)) {
-            return intValue;
-          }
-
-          input.$setValidity('mustBeInteger', false);
-        } else {
-          input.$setValidity('number', false);
-        }
-      } catch (e) {
-        input.$setValidity('number', false);
-        input.$setValidity('mustBeInteger', false);
-      }
-
-      return null;
-    }
-
-    check(input) {
-      if (input != null) {
-        this[`${input.$name}Check`](input);
-        this.setValidity();
-      }
-    }
-
-    checkPasswordAge() {
-      this.check(this.serviceForm.maxPasswordAge);
-      this.check(this.serviceForm.minPasswordAge);
-      this.check(this.serviceForm.passwordHistoryCount);
     }
   },
 );
