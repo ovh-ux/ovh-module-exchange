d0eb718a8432859fbfe5fd16cb15ac274d498801
cyril.biencourt <cyril.biencourt@corp.ovh.com>
2019-08-07 13:59:52 +0200

    feat(exchange.mfa): disable for dedicated offer

--- src/exchange/account/mfa/account-mfa.controller.js
+++ src/exchange/account/mfa/account-mfa.controller.js
@@ -1,18 +1,45 @@
 angular.module('Module.exchange.controllers')
   .controller('ExchangeAccountMfaCtrl', class ExchangeAccountMfaCtrl {
-    constructor($scope, $translate, Exchange, exchangeAccount, messaging, navigation) {
+    constructor(
+      $q,
+      $scope,
+      $timeout,
+      $translate,
+      Exchange,
+      exchangeAccount,
+      messaging,
+      navigation,
+    ) {
+      this.$q = $q;
       this.$scope = $scope;
+      this.$timeout = $timeout;
       this.$translate = $translate;
       this.exchangeAccount = exchangeAccount;
       this.messaging = messaging;
       this.navigation = navigation;
       this.exchangeService = Exchange.value;
+      this.Exchange = Exchange;
       $scope.submit = () => this.submit();
     }
 
     $onInit() {
       this.account = this.navigation.currentActionData.account;
       this.action = this.navigation.currentActionData.action;
+
+      this.Exchange.getExchangeServer(
+        this.exchangeService.organization,
+        this.exchangeService.domain,
+      ).then((server) => {
+        this.server = server;
+      }).catch((error) => {
+        this.messaging.writeError(
+          this.$translate.instant('exchange_mfa_error', {
+            error: error.message,
+          }),
+        );
+
+        this.$scope.resetAction();
+      });
     }
 
     create() {
@@ -45,25 +72,33 @@ angular.module('Module.exchange.controllers')
     }
 
     submit() {
-      let promise = null;
+      const serverMfaPromise = this.server.owaMfa
+        ? this.$q.resolve(true)
+        : this.Exchange.updateExchangeServer(
+          this.exchangeService.organization,
+          this.exchangeService.domain,
+          { owaMfa: true },
+        );
+
+      let accountMfaPromise = null;
       let messages = {};
       switch (this.action) {
         case 'RESET':
-          promise = this.reset();
+          accountMfaPromise = this.reset();
           messages = {
             success: 'exchange_reset_mfa_success',
             error: 'exchange_reset_mfa_error',
           };
           break;
         case 'DISABLE':
-          promise = this.disable();
+          accountMfaPromise = this.disable();
           messages = {
             success: 'exchange_disable_mfa_success',
             error: 'exchange_disable_mfa_error',
           };
           break;
         case 'ENABLE':
-          promise = this.account.mfa.status === 'NOT_CREATED' ? this.create() : this.enable();
+          accountMfaPromise = this.account.mfa.status === 'NOT_CREATED' ? this.create() : this.enable();
           messages = {
             success: 'exchange_enable_mfa_success',
             error: 'exchange_enable_mfa_error',
@@ -73,16 +108,20 @@ angular.module('Module.exchange.controllers')
           this.$scope.resetAction();
       }
 
-      return promise.then(() => {
-        this.messaging.writeSuccess(
-          this.$translate.instant(messages.success),
-        );
-      }).catch((error) => {
-        this.messaging.writeError(
-          this.$translate.instant(messages.error, {
-            error: error.message,
-          }),
-        );
-      }).finally(() => this.$scope.resetAction());
+      return serverMfaPromise
+        .then(() => this.$timeout(2000))
+        .then(() => accountMfaPromise)
+        .then(() => {
+          this.messaging.writeSuccess(
+            this.$translate.instant(messages.success),
+          );
+        }).catch((error) => {
+          this.messaging.writeError(
+            this.$translate.instant(messages.error, {
+              error: error.message,
+            }),
+          );
+        })
+        .finally(() => this.$scope.resetAction());
     }
   });
